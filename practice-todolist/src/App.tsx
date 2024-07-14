80629f5d92ce3bc5a15d62822b0e614b37a009ff
村中信隆 <chanayataka2324@gmail.com>
2024-07-14 19:45:27 +0900

    追加されるタスクの見た目準備

--- practice-todolist/src/App.tsx
+++ practice-todolist/src/App.tsx
@@ -4,8 +4,15 @@ function App() {
   return (
     <>
       <h1 className="text-5xl mb-8 font-bold">Todoリスト with Typescript</h1>
-      <input className="border border-black py-2 rounded-lg px-1.5"></input>
-      <button className="border border-black">作成</button>
+      <div>
+        <input className="border border-black py-2 rounded-lg px-1.5"></input>
+        <button className="border border-black">作成</button>
+      </div>
+      <div className="my-5">
+        <input className="border border-black py-2 rounded-lg px-1.5 mr-2"></input>
+        <input type="checkbox" className="mr-2"></input>
+        <button className="border border-black p-1">削</button>
+      </div>
     </>
   );
 }
