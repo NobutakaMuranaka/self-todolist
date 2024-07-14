8f4691787aebbfc28270a62b9011fe0cd9295aa2
村中信隆 <chanayataka2324@gmail.com>
2024-07-14 18:55:21 +0900

    タイトルとフォームの作成、Tailwindのセットアップ

---
+++ practice-todolist/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-5xl mb-8 font-bold">Todoリスト with Typescript</h1>
+      <input className="border border-black py-2 rounded-lg px-1.5"></input>
+      <button className="border border-black">作成</button>
+    </>
+  );
+}
+
+export default App;
